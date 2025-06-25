bf3957cf272e6b4173232ce1235f50e497978a86
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2025-06-25 12:11:55 -0700

    Update embed.js

--- embed.js
+++ embed.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", async () => {
   }
 
   try {
-    const response = await fetch(apiUrl);
+    const response = await fetch(`${apiUrl}/g.json`);
 
     if (!response.ok)
       throw new Error(`Failed to fetch games JSON: ${response.statusText}`);
@@ -37,7 +37,7 @@ document.addEventListener("DOMContentLoaded", async () => {
       </div>
     `).join("");
 
-     /*   const response2 = await fetch(apiUrl2);
+       const response2 = await fetch(`${apiUrl2}/g.json`);
 
     if (!response2.ok)
       throw new Error(`Failed to fetch games JSON: ${response2.statusText}`);
@@ -63,7 +63,7 @@ document.addEventListener("DOMContentLoaded", async () => {
         <img src="${apiUrl2}/images/${game.alt}.webp" alt="${game.title} thumbnail" style="width: clamp(50px, 120px, 240px); border-radius:8px; height: 120px; object-fit: cover;" onerror="this.onerror=null;this.src='https://placehold.co/200x120/cccccc/333333?text=No+Image';" />
         <h3 style="margin-top:10px; font-size:18px; color: #333333;">${game.title}</h3>
       </div>
-    `).join("");*/
+    `).join("");
   } catch (err) {
     target.innerHTML = "<p style='color:red; text-align: center; font-family: sans-serif;'>Error loading games. Please try again later.</p>";
     console.error("Error loading games:", err);
